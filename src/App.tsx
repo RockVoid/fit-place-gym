9f2289ebc02d80e25ce69826b1804fa1aac7bd8e
Roque <devroque18@gmail.com>
2023-09-27 14:28:08 -0300

    [

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+import { Navbar } from "./scenes/navbar";
+import { SelectedPage } from "./shared/types";
+
 export default function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </h1>
   )
 }
