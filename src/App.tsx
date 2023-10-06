ebc9b3d719182f479914b00e2733a8a920d2f545
Roque <devroque18@gmail.com>
2023-10-06 11:44:00 -0300

    [FEAT]: Smooth Scroll and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navbar } from "./scenes/navbar";
 import { SelectedPage } from "./shared/types";
 
 export default function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if(window.scrollY !== 0) {
+        setIsTopOfPage(false);
+      } 
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      </h1>
+    </>
   )
 }
